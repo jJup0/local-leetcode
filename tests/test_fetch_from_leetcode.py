076c6ac01cc5a8e7ef46161a469cd35c650988d2
Jakob Roithinger <jake.roithinger@gmail.com>
2024-03-17 10:33:22 +0100

    split up test for fetch using selenium and requests

--- tests/test_fetch_from_leetcode.py
+++ tests/test_fetch_from_leetcode.py
@@ -1,16 +1,27 @@
-import pytest
+import logging
+
+import selenium.common.exceptions
 
 from local_leetcode.leetcode_fetchers import (
     ClassicRequestsFetcher,
-    LeetCodeFetcher,
     SeleniumRequestsFetcher,
 )
 
+logger = logging.getLogger(__name__)
 
-@pytest.mark.parametrize(
-    "fetcher", [ClassicRequestsFetcher(), SeleniumRequestsFetcher()]
-)
-def test_fetch_using_requests(fetcher: LeetCodeFetcher):
-    slug = fetcher.get_daily_question_slug()
+
+def test_fetch_using_requests() -> None:
+    slug = ClassicRequestsFetcher().get_daily_question_slug()
     assert type(slug) is str
     assert len(slug) > 0
+
+
+def test_fetch_using_selenium() -> None:
+    try:
+        slug = SeleniumRequestsFetcher().get_daily_question_slug()
+        assert type(slug) is str
+        assert len(slug) > 0
+    except selenium.common.exceptions.SessionNotCreatedException:
+        logger.warning(
+            "Selenium could not run, ignore this message if running on a server."
+        )
