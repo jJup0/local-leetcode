b0648789b438e95c84346eb512c6fd8c7a195dfa
Jakob Roithinger <jake.roithinger@gmail.com>
2024-03-18 20:26:29 +0100

    added ListNode class

---
+++ src/local_leetcode/leetcode_special_classes/ListNode.py
@@ -0,0 +1,32 @@
+from typing import Any, Optional
+
+
+class ListNode:
+    detection_str = (
+        "# Definition for singly-linked list."
+        "# class ListNode:"
+        "#     def __init__(self, val=0, next=None):"
+        "#         self.val = val"
+        "#         self.next = next"
+    )
+
+    @staticmethod
+    def fromList(l: list[Any]) -> "ListNode | None":
+        dummy = curr = ListNode()
+        for x in l:
+            curr.next = ListNode(x)
+            curr = curr.next
+        return dummy.next
+
+    def __init__(self, val: int = 0, next: "ListNode | None" = None):
+        self.val = val
+        self.next: Optional["ListNode"] = next
+
+    def __repr__(self) -> str:
+        l: list[Any] = []
+        head = self
+        while head:
+            l.append(head.val)
+            head = head.next
+
+        return str(l)
