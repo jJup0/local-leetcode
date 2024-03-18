aa490488abcfca3731de7fd9b0eee8ca9c38e4fa
Jakob Roithinger <jake.roithinger@gmail.com>
2024-03-18 20:26:49 +0100

    removed test_code_conversion

--- tests/test_code_conversion.py
+++
@@ -1,9 +0,0 @@
-from local_leetcode.fetch_from_leetcode import clean_code
-
-
-def test_clean_code():
-    str_in = "class Solution:\n    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:"
-
-    expected_out = "class Solution:\n    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:"
-
-    assert clean_code(str_in) == expected_out
